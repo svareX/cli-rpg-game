2e1488a59f4c039483235c1e2ef81ddff2a2b04d
Lukáš Temelkov <xtemelko@mendelu.cz>
2023-12-01 18:02:04 +0100

    Declared class Merchant attributes and functions

---
+++ gameRPG/include/Merchant.h
@@ -0,0 +1,26 @@
+#ifndef GAMERPG_MERCHANT_H
+#define GAMERPG_MERCHANT_H
+
+#include <vector>
+#include "Item.h"
+#include "Map.h"
+using namespace std;
+
+struct Product{
+    Item* item;
+    float goldAmount;
+};
+class Merchant{
+    int merchantX;
+    int merchantY;
+    std::vector <Product> items;
+    Map& map; //merchant associated with map
+public:
+    Merchant(Map& gameMap);
+    void displayShop();
+    void buyItem();
+    void sellItem();
+    // add global function addToMap to GameEngine, parameters: coordinates X&Y and an object
+};
+
+#endif //GAMERPG_MERCHANT_H
